c0600c923b7d7b6e1036342940cef0dc4a1fcde4
Mini <mini@Minis-Mac-mini.local>
2018-12-13 17:15:14 +0200

    created new header

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './Header'
+import About from './About'
+import Contact from './Contact'
+import Home from './Home'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <Router>
+        <div>
+           <Header />
+           <Route exact path = "/" component={Home}/>
+           <Route path="/About" component={About} />
+           <Route path="/Contact" component={Contact} />
+
+        </div>
+
+      </Router>
+    )
   }
 }
 
 export default App;
+
+
